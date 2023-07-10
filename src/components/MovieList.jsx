a33ddb22df137b63daead39b007a19499b9a4c28
8ohowb <8ohowb@naver.com>
2023-07-10 15:00:38 +0900

    선호작 추가와 제거 표시

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,9 +6,14 @@ const MovieList = (props) => {
       {props.movies.map((movie) => (
         <div className="image-container d-flex m-3" key={movie.imdbID}>
           <img src={movie.Poster} alt="movie"></img>
-          <div className="overlay d-flex align-items-center justify-content-center">
-            <span className="me-2">선호작 추가</span>
-            <span>🧡</span>
+          <div
+            className="overlay d-flex align-items-center justify-content-center"
+            onClick={() => props.handleClick(movie)}
+          >
+            <span className="me-2">
+              {props.addMovie ? "선호작 추가" : "선호작 제거"}
+            </span>
+            <span>{props.addMovie ? "🧡" : "❌"}</span>
           </div>
         </div>
       ))}
