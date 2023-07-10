cd97b389dcc6ee66dcb505b4c175c5c1566cfc04
8ohowb <8ohowb@naver.com>
2023-07-10 11:49:27 +0900

    서치박스 입력시 업데이트

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "The Amazing Spider-Man",
-      Year: "2012",
-      imdbID: "tt0948470",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Amazing Spider-Man 2",
-      Year: "2014",
-      imdbID: "tt1872181",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOTA5NDYxNTg0OV5BMl5BanBnXkFtZTgwODE5NzU1MTE@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Amazing World of Gumball",
-      Year: "2011–2019",
-      imdbID: "tt1942683",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYWU1YTA4OGUtNjcxMC00ZTllLTgxYWUtY2U5NmViZTU0MmNjXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
+
+  //검색어로 영화데이터 요청 async/await는 쌍으로 사용
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=6bfc4a64`;
+    //자바스크립트는 비동기므로 await를 붙여서 영화데이터를 다받고 다음코드 실행
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+      //결과가 있을경우!
+      setMovies(responseJson.Search);
+    }
+  };
+
+  //검색어가 바뀔 시 한번 함수 실행
+  useEffect(() => {
+    if (searchValue.length > 3) {
+      getMovieRequest(searchValue);
+    }
+  }, [searchValue]);
+
   return (
     <div className="container-fluid movie-app">
+      <div className="row align-items-center my-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
